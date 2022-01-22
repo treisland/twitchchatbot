242c8b83ebcc99f5139d0ace66e1a0c05dc0020d
Tre Island <t.island84@gmail.com>
2022-01-22 14:48:38 -0700

    added timed messages

--- bot.js
+++ bot.js
@@ -23,6 +23,7 @@ const client = new tmi.client(opts);
 client.on('message', onMessageHandler);
 client.on('connected', onConnectedHandler);
 
+
 // Connect to Twitch:
 client.connect();
 
@@ -31,7 +32,6 @@ function onMessageHandler(channel,tags, msg, self) {
     if (self) { return; } // Ignore messages from the bot
 
     // Remove whitespace from chat message
-    console.log(msg);
     const commandName = msg.trim();
 
     try {
@@ -59,4 +59,43 @@ function timedMessages(){
 // Called every time the bot connects to Twitch chat
 function onConnectedHandler(addr, port) {
     console.log(`* Connected to ${addr}:${port}`);
+    timedMessages()
+}
+
+function timer_github(){
+    setInterval(() => {
+        client.say(process.env.CHANNEL_NAME, "");
+    }, 5000);
+}
+
+function timedMessages(){
+    const channel = process.env.CHANNEL_NAME
+
+    try {
+        let fileContents = fs.readFileSync('./timedmessages.json', 'utf8');
+        let data = JSON.parse(fileContents);
+
+
+        //console.log(data[1])
+        for(var i = 0; i < data.length; i++) {
+            (function(i){
+                var obj = data[i];
+                var timer=data[i].seconds * 1000
+
+                if (obj.enabled){
+                    
+                    if (obj.onStart){
+                        client.say(channel, obj.message)
+                    }
+
+                    setInterval(() => {
+                        client.say(channel, obj.message);
+                    }, timer);
+                }
+            })(i)
+        }
+
+    } catch (e) {
+        console.log(e);
+    }
 }
